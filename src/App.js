7066b4662915128135983fb9ab8f21faa729d79b
root <hish.b92@gmail.com>
2020-03-13 12:48:16 +0500

    Add: Prop Types

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
-  rener() {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      },
+    ]
+  }
+  render() {
     return (
-      <div className='App'>
-        <Todos />
+      <div className="App">
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
