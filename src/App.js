80946b66c3d67ffdc169caf8630382b346ab5046
root <hish.b92@gmail.com>
2020-03-14 13:03:03 +0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/layout/AddTodo';
 
 import './App.css';
 
@@ -42,9 +43,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="container">
         <Header />
+        <AddTodo />
         <Todos todos={this.state.todos} markComplete={this.markComplete}
         delTodo={this.delTodo} />
+      </div>
       </div>
     );
   }
